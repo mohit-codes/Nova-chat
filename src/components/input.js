570aa1d135829bbc9439d3a0138dadbfc1567805
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-22 18:56:21 +0530

    feat: reusable input component

---
+++ src/components/input.js
@@ -0,0 +1,11 @@
+export const Input = ({ callback, placeholder, value, type }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={callback}
+      className="max-w-60 md:w-72 bg-transparent border-2 px-3 py-2 outline-none border-black rounded-md"
+    />
+  );
+};
