1a66fec2ec9bb2df4b2339d9aac7092879924274
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-22 18:59:01 +0530

    feat: reach router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { Router } from "@reach/router";
+import { Home, Login } from "./pages/index";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Login path="/" />
+        <Home path="home" />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
