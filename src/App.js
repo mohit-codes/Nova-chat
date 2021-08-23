cfde8ea07772e7d1671ff61578dad119f895cdcc
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-23 19:13:59 +0530

    feat: singup page route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Router } from "@reach/router";
-import { Home, Login } from "./pages/index";
+import { Home, Login, Signup } from "./pages/index";
 function App() {
   return (
     <div className="App">
       <Router>
         <Login path="/" />
         <Home path="home" />
+        <Signup path="signup" />
       </Router>
     </div>
   );
