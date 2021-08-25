62e9d1c7d31abe999b63f3e9dd8b4d30a7e10bfd
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-25 18:57:42 +0530

    feat: private route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Router } from "@reach/router";
-import { Home, Login, Signup } from "./pages/index";
+import { Login, Signup } from "./pages/index";
+import { PrivateRoute } from "./components/PrivateRoute";
 function App() {
   return (
     <div className="App">
       <Router>
         <Login path="/" />
-        <Home path="home" />
+        <PrivateRoute path="home" />
         <Signup path="signup" />
       </Router>
     </div>
