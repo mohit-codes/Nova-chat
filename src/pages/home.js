8c5030fe007d66cc90027dd4d76e8ad4e39573f0
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-27 18:55:38 +0530

    feat: components added to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {
+  ChatCardWrapper,
+  CreateMenu,
   LeftUpperHeader,
   RightSection,
   SavedMessagesTile,
+  StartConversation,
 } from "../components/index";
 import { useAuth } from "../context/authProvider";
 import { useAxiosGet } from "../hooks/useAxiosGet";
+import { BASE_URL } from "../utils/utils";
 
 export const Home = () => {
   const [rightSide, setRightSide] = useState(null);
   const { user } = useAuth();
   const [showStartMessage, setShowStartMessage] = useState(false);
-  const { data: recipients } = useAxiosGet("recipients", user._id);
+  const { data: recipients, addItem: addRecipient } = useAxiosGet(
+    "recipients",
+    user._id
+  );
   const { data: groups } = useAxiosGet("groups", user._id);
-  console.log(recipients, groups);
-
+  console.log(recipients);
   return (
     <div className="min-h-screen bg-background px-36 pt-14">
       <div className="mr-auto ml-auto flex h-600 w-full bg-back rounded-md">
@@ -28,34 +36,24 @@ export const Home = () => {
                 placeholder="search"
               />
             </div>
-            <div className="flex mb-3 px-3 justify-between">
-              <button
-                onClick={() => setShowStartMessage(true)}
-                className="rounded-full px-2 py-1 shadow-md"
-              >
-                <i className="fa fa-comment mr-2"></i>New Message
-              </button>
-              <button className="rounded-full  px-2 py-1 shadow-md">
-                <i className="fa fa-users mr-2"></i>New Group
-              </button>
-            </div>
+            <CreateMenu setShowStartMessage={setShowStartMessage} />
             {showStartMessage && (
-              <div className="w-full px-2 mb-2">
-                <input
-                  type="text"
-                  className="rounded-full w-full my-2 px-3 py-1 shadow-md"
-                  placeholder="Recipient Email"
-                />
-                <button className="rounded-full px-3 py-1 shadow-md">
-                  Start
-                </button>
-                <i
-                  onClick={() => setShowStartMessage(false)}
-                  className="fa fa-close ml-2"
-                ></i>
-              </div>
+              <StartConversation
+                addRecipient={addRecipient}
+                setShowStartMessage={setShowStartMessage}
+              />
             )}
             <SavedMessagesTile callback={() => setRightSide("saved")} />
+            {recipients?.map((recipient) => {
+              return (
+                <ChatCardWrapper
+                  callback={() => setRightSide(recipient)}
+                  key={recipient._id}
+                >
+                  {recipient.name}
+                </ChatCardWrapper>
+              );
+            })}
           </div>
         </div>
         <div className="w-full h-full">
@@ -64,7 +62,7 @@ export const Home = () => {
               <p className="text-2xl text-white font-bold">Nova Chat</p>
             </div>
           ) : (
-            <RightSection receiverId={rightSide} />
+            <RightSection recipient={rightSide} />
           )}
         </div>
       </div>
