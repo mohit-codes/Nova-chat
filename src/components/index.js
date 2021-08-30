a954f46eed6c5e42e034cbad7ae45edd18f40bf7
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-30 19:14:55 +0530

    feat: exports

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { RightSection } from "./Home/RightSection";
 export { StartConversation } from "./Home/StartConversation";
 export { CreateMenu } from "./Home/CreateMenu";
 export { ChatCardWrapper } from "./Home/ChatCardWrapper";
+export { LeftSection } from "./Home/LeftSection";
+export { Spinner } from "./Spinner";
