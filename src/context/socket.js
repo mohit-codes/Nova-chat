35273a45a525cdc908a6b02c2395e2ca9c9b90f8
mohit kushwaha <kushwahamohit000@gmail.com>
2021-09-01 18:56:26 +0530

    feat: useContext

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,7 @@
 import socketio from "socket.io-client";
 import { BASE_URL } from "../utils/utils";
-import React from "react";
+import React, { useContext } from "react";
 
 export const socket = socketio(BASE_URL);
 export const SocketContext = React.createContext();
+export const useSocket = () => useContext(SocketContext);
