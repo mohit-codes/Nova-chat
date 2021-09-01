51fd8150f56483377312fd811502c986f08d9211
mohit kushwaha <kushwahamohit000@gmail.com>
2021-09-01 18:55:20 +0530

    feat: emojis feature | messages by Date

--- src/components/Home/RightSection.js
+++ src/components/Home/RightSection.js
@@ -7,6 +7,7 @@ import { useData } from "../../context/dataProvider";
 import { useSocket } from "../../context/socket";
 import { BASE_URL, scrollBottom } from "../../utils/utils";
 import { Spinner } from "../Spinner";
+const emojis = require("emojis-list").slice(301);
 
 export const RightSection = ({ setRightSide, recipient }) => {
   const { user, setUser } = useAuth();
@@ -17,11 +18,11 @@ export const RightSection = ({ setRightSide, recipient }) => {
   const [loading, setLoading] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const [isOnline, setIsOnline] = useState(false);
+  const [showEmojis, setShowEmojis] = useState(false);
   const socket = useSocket();
-
+  let date;
   useEffect(() => {
     socket.on("message", (info) => {
-      console.log(info);
       setMessages((prevState) => [...prevState, info]);
       scrollBottom("messages");
     });
@@ -35,7 +36,6 @@ export const RightSection = ({ setRightSide, recipient }) => {
         userId: user._id,
         receiverId: recipient._id,
       });
-      console.log(chats);
       setMessages(chats);
       setLoading(false);
       scrollBottom("messages");
@@ -80,7 +80,6 @@ export const RightSection = ({ setRightSide, recipient }) => {
       message: message,
     });
   };
-
   return (
     <div className="shadow-lg h-full flex flex-col">
       <div className=" z-10 w-full px-3 py-2 shadow-md h-12 rounded-tr-md  bg-white font-medium">
@@ -113,19 +112,36 @@ export const RightSection = ({ setRightSide, recipient }) => {
               <Spinner />
             </div>
           ) : (
-            messages.map((msg) => {
+            messages.map((msg, index) => {
               const time = dayjs(msg.createdAt).format("h.mm a");
+              const currentDate = dayjs(msg.createdAt).format("DD-MM-YYYY");
+              let showDate =
+                index === 0 ? true : date === currentDate ? false : true;
+              date =
+                index === 0
+                  ? currentDate
+                  : date === currentDate
+                  ? date
+                  : currentDate;
               return (
-                <div
-                  key={msg.messageId}
-                  className={`mb-3 w-min whitespace-nowrap py-2 px-3 rounded-3xl  shadow-xl  ${
-                    msg.sender.name === user.name
-                      ? "ml-auto bg-background text-white rounded-br-none"
-                      : "text-black bg-white rounded-bl-none"
-                  }`}
-                >
-                  <span className="mr-2">{msg.message}</span>{" "}
-                  <span className="text-exs">{time}</span>
+                <div key={msg.messageId}>
+                  {showDate && (
+                    <p className="w-full flex justify-center">
+                      <span className="shadow-lg rounded-full py-1 px-2 font-normal">
+                        {date}
+                      </span>
+                    </p>
+                  )}
+                  <div
+                    className={`mb-3 w-min whitespace-nowrap py-2 px-3 rounded-3xl  shadow-xl  ${
+                      msg.sender.name === user.name
+                        ? "ml-auto bg-background text-white rounded-br-none"
+                        : "text-black bg-white rounded-bl-none"
+                    }`}
+                  >
+                    <span className="mr-2">{msg.message}</span>{" "}
+                    <span className="text-exs">{time}</span>
+                  </div>
                 </div>
               );
             })
@@ -145,9 +161,30 @@ export const RightSection = ({ setRightSide, recipient }) => {
           })
         )}
       </div>
-      <div className=" flex bg-white w-full rounded-br-md">
+      <div className="relative flex bg-white w-full rounded-br-md justify-end px-5">
+        <button onClick={() => setShowEmojis(!showEmojis)}>
+          <i className="far fa-smile text-xl"></i>
+        </button>
+        {showEmojis && (
+          <div
+            className="absolute
+           bg-back flex flex-wrap left-0 -top-52 overflow-y-auto w-96 h-52"
+          >
+            {emojis.map((emoji, index) => {
+              return (
+                <div
+                  className="p-1 cursor-pointer"
+                  key={index}
+                  onClick={() => setMessage(message + " " + emoji)}
+                >
+                  {emoji}
+                </div>
+              );
+            })}
+          </div>
+        )}
         <form
-          className="flex items-center py-2"
+          className="flex items-center py-2 w-full"
           onSubmit={(e) =>
             recipient === "saved" ? saveMsgHandler(e) : sendHandler(e)
           }
@@ -157,13 +194,13 @@ export const RightSection = ({ setRightSide, recipient }) => {
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             placeholder="Type your message here"
-            className=" max-h-40 rounded-md shadow-md bg-transparent py-1  px-4 w-500 ml-3 "
+            className=" max-h-40 rounded-md shadow-md bg-transparent py-1  px-4 w-full ml-3 "
           />
 
           <button
             type="submit"
             disabled={message === ""}
-            className={`rounded-full shadow-lg px-2 h-10 ml-3 ${
+            className={`rounded-full whitespace-nowrap shadow-lg px-2 h-10 ml-3 ${
               message === "" ? "cursor-not-allowed" : "cursor-pointer"
             }`}
           >
