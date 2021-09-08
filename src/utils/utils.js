1cf9cce33c57769fa2ffebe165c799854e16848c
mohit kushwaha <kushwahamohit000@gmail.com>
2021-09-08 18:50:58 +0530

    feat: util services

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,23 @@
+import axios from "axios";
+
 export const BASE_URL = "http://localhost:8080";
 
 export function scrollBottom(id) {
   document.getElementById(id).scrollTop =
     document.getElementById(id).scrollHeight;
+}
+
+export async function fetchChats(userId, recipientId) {
+  const {
+    data: { messages: chats },
+  } = await axios.post(`${BASE_URL}/messages/get_messages`, {
+    userId: userId,
+    receiverId: recipientId,
+  });
+  return chats;
+}
+
+export async function deleteUser(userId) {
+  const { data: response } = await axios.delete(`${BASE_URL}/users/${userId}`);
+  return response;
 }
