06d6badeee39616356aa0dd7c2e374d4551eb804
mohit kushwaha <kushwahamohit000@gmail.com>
2021-09-14 18:50:03 +0530

    feat: loading spinner | admin label on member

--- src/components/Home/GroupMembers.js
+++ src/components/Home/GroupMembers.js
@@ -1,17 +1,23 @@
 /* eslint-disable no-unused-vars */
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Spinner } from "..";
+import { useAuth } from "../../context/authProvider";
 import { BASE_URL } from "../../utils/utils";
 
 export const GroupMembers = ({ group, isAdmin }) => {
+  const { user } = useAuth();
   const [members, setMembers] = useState([]);
   const [showAddMemberForm, setShowAddMemberForm] = useState(false);
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   useEffect(async () => {
+    setLoading(true);
     const { data: data } = await axios.get(
       `${BASE_URL}/groups/members/${group._id}`
     );
+    setLoading(false);
     setMembers(data.members);
   }, []);
 
@@ -64,7 +70,7 @@ export const GroupMembers = ({ group, isAdmin }) => {
         </div>
       )}
       <div className="">
-        <div className="border-2 border-gray-200 mt-4 px-3 py-2">
+        <div className="border-2 border-gray-200 bg-white mt-4 px-3 py-2">
           <div className="flex justify-between items-center">
             <span>Members</span>
             {isAdmin && (
@@ -75,13 +81,28 @@ export const GroupMembers = ({ group, isAdmin }) => {
             )}
           </div>
         </div>
-        {members.map((member) => {
-          return (
-            <div className="px-3" key={member._id}>
-              {member.name}
-            </div>
-          );
-        })}
+        {loading ? (
+          <div className="flex justify-center mt-2">
+            {" "}
+            <Spinner />{" "}
+          </div>
+        ) : (
+          members.map((member) => {
+            return (
+              <div className="px-3 py-2 border-2 border-gray" key={member._id}>
+                <p>
+                  {member.name}{" "}
+                  {user._id === member._id && (
+                    <span className="border-2 border-black rounded-xl px-1 float-right">
+                      admin
+                    </span>
+                  )}
+                </p>
+                <p className="text-sm">{member.email}</p>
+              </div>
+            );
+          })
+        )}
       </div>
     </>
   );
