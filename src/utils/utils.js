dc785f00b58fb6e7e64298e22b826a622d8a84d8
mohit kushwaha <kushwahamohit000@gmail.com>
2021-09-30 18:36:36 +0530

    feat: fetch saved message service

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,6 +7,13 @@ export function scrollBottom(id) {
     document.getElementById(id).scrollHeight;
 }
 
+export async function fetchSavedMessages(id) {
+  const {
+    data: { savedMessages: savedMessages },
+  } = await axios.get(`${BASE_URL}/users/savedMessages/${id}`);
+  return savedMessages;
+}
+
 export async function fetchChats(userId, recipientId, endPoint) {
   const data = {
     userId: userId,
