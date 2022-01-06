6e4a0f93b16b9a64bdcb2785c48118cd4f0f808f
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-06 16:25:20 +0530

    fix: fixed missing bracket

--- src/components/Home/ChatCardWrapper.js
+++ src/components/Home/ChatCardWrapper.js
@@ -1,8 +1,6 @@
-export const ChatCardWrapper = ({ children) => {
+export const ChatCardWrapper = ({ children }) => {
   return (
-    <div
-      className="py-4 px-5 h-14 border-t-2 border-b-2 border-gray-200 cursor-pointer"
-    >
+    <div className="py-4 px-5 h-14 border-t-2 border-b-2 border-gray-200 cursor-pointer">
       {children}
     </div>
   );
