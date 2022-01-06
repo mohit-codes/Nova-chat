477985de47b2d3c1eb76ba8aa6411d9af4aa8dca
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-06 16:30:03 +0530

    refactor: Link to nested route

--- src/components/Home/SavedMessagesTile.js
+++ src/components/Home/SavedMessagesTile.js
@@ -1,13 +1,18 @@
+import { Link } from "@reach/router";
 import React from "react";
+import { useAuth } from "../../context/authProvider";
 import { ChatCardWrapper } from "./ChatCardWrapper";
 
-export const SavedMessagesTile = ({ callback }) => {
+export const SavedMessagesTile = () => {
+  const { user } = useAuth();
   return (
-    <ChatCardWrapper callback={callback}>
-      <span className="rounded-full px-2 py-1 border-2 border-gray-400">
-        <i className="far fa-bookmark"></i>
-      </span>{" "}
-      Saved Messages
-    </ChatCardWrapper>
+    <Link to={user._id} state={{ type: "saved" }}>
+      <ChatCardWrapper>
+        <span className="rounded-full px-2 py-1 border-2 border-gray-400">
+          <i className="far fa-bookmark"></i>
+        </span>{" "}
+        Saved Messages
+      </ChatCardWrapper>
+    </Link>
   );
 };
