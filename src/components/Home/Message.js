0c9ca7d2d86445f4d98a00796e38159ac91bf69a
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-22 16:58:27 +0530

    style: added spacing

--- src/components/Home/Message.js
+++ src/components/Home/Message.js
@@ -14,7 +14,7 @@ const Message = ({ msg, isAdmin, isGroup, messageDeleteHandler }) => {
     <div
       onMouseEnter={() => setShowMessageOptions(true)}
       onMouseLeave={() => setShowMessageOptions(false)}
-      className="flex w-full relative items-end"
+      className="flex w-full relative items-end my-2"
     >
       <div
         className={`relative w-min whitespace-nowrap py-2  px-3 rounded-3xl shadow-xl  ${
